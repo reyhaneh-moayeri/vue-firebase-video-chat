1cb8c789485cc74c1f1689d029a7d784be609307
reyhaneh <reyhanehrm333@gmail.com>
2022-02-23 15:47:24 +0330

    used tailwind to style login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,48 +1,44 @@
 <template>
   <div>
     <form class="mt-3" @submit.prevent="login">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6">
-            <div class="card bg-light">
-              <div class="card-body">
-                <h3 class="font-weight-light mb-3">Log in</h3>
-                <section class="form-group">
-                  <div class="col-12 alert alert-danger px-3" v-if="error">
-                    {{ error }}
-                  </div>
-                  <label class="form-control-label sr-only" for="Email">Email</label>
-                  <input
-                    required
-                    class="form-control"
-                    type="email"
-                    id="email"
-                    placeholder="Email"
-                    v-model="email"
-                  />
-                </section>
-                <section class="form-group">
-                  <input
-                    required
-                    class="form-control"
-                    type="password"
-                    placeholder="Password"
-                    v-model="password"
-                  />
-                </section>
-                <div class="form-group text-right mb-0">
-                  <button class="btn btn-primary" type="submit">Log in</button>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col max-w-xl mx-auto">
+        <div class="alert alert-danger px-3" v-if="error">
+          {{ error }}
         </div>
+        <div class="mb-4">
+          <label class="block text-grey-darker text-sm font-bold mb-2" for="email"> Email </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+            id="email"
+            type="text"
+            placeholder="Email"
+            v-model="email"
+          />
+        </div>
+        <div class="mb-6">
+          <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+            Password
+          </label>
+          <input
+            class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+            id="password"
+            type="password"
+            placeholder="******************"
+            v-model="password"
+          />
+          <p class="text-red text-xs italic">Please choose a password.</p>
+        </div>
+        <div class="flex items-center justify-between">
+          <button class="text-white bg-blue-500 font-bold py-2 px-4 rounded" type="submit">
+            Sign In
+          </button>
+        </div>
+        <p class="text-center mt-2">
+          or
+          <router-link to="/register">register</router-link>
+        </p>
       </div>
     </form>
-    <p class="text-center mt-2">
-      or
-      <router-link to="/register">register</router-link>
-    </p>
   </div>
 </template>
 

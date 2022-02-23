5496e01be2462a811da3ef59074e37987e19f5af
reyhaneh <reyhanehrm333@gmail.com>
2022-02-23 13:48:51 +0330

    added tailwind and style home page

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/login">Login</router-link>
+  <div class="nav flex justify-between align-center bg-red-500 p-4 text-white">
+    <h2 class="font-bold text-xl">Video Chat</h2>
+    <div class="justify-self-end">
+      <router-link to="/">Home</router-link>
+      <router-link class="mx-3" to="/login">Login</router-link>
+    </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="home">
-    <h3>this is home page</h3>
-    <p>welcome {{ user }}</p>
+  <div class="home text-center container mx-auto">
+    <p v-if="user" class="mt-3">
+      welcome <span class="text-blue-600">{{ user }}</span>
+    </p>
+    <h1 class="mt-8 text-5xl px-6 font-extrabold text-red-500 tracking-tight">
+      Start a video chat
+    </h1>
+    <img class="w-32 mt-3 mx-auto" src="http://pixelprowess.com/i/powship.svg" alt="POW Ship" />
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
 import db from '../db.js'
